b9ef219b46cd72b4e0087cdd6b14108811840993
pcf-pro <korobovdenis7@gmail.com>
2019-12-13 12:09:32 +0300

    1

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,11 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    #Post.objects.get(pk=pk)
+
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 # Create your views here.
