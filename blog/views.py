7e5f951c0f25fc87e2fde409caedef8c4147a7a0
pcf-pro <korobovdenis7@gmail.com>
2019-12-13 12:11:18 +0300

    12

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
     Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 # Create your views here.
