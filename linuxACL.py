2ed5338a19236dee3121c485640eaf93600f6667
pcf-pro <46861522+pcf-pro@users.noreply.github.com>
2019-12-16 16:45:41 +0300

    linux

---
+++ linuxACL.py
@@ -0,0 +1,40 @@
+r"""
+Usage example
+for help
+$ python linuxACL.py -h
+"""
+
+import argparse
+import os
+
+def greate():
+    os.system('pwd')
+
+def sets():
+    pass
+
+def pars():
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Программа для парсинга данных ACL',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument("--greate", required=False, action="store_true",
+                        help='Создать файл с правами')
+    parser.add_argument("--sets", required=False, action="store_true", help='установить права')
+    parser.add_argument('--pars', required=False, action='store true',
+                        help='Распарсить данные и вывести на экран')
+
+    args = parser.parse_args()
+
+    if args.greate:
+        greate()
+
+    elif args.sets:
+        sets()
+    elif args.pars:
+        pars()
